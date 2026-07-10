7f1dafed0597fed0851c2613d5efd8ab5af5c5cb
semihtoktas2018-byte <semihtoktas2018@gmail.com>
2026-07-11 00:50:28 +0300

    Update BestSellers.tsx

--- src/pages/BestSellers.tsx
+++ src/pages/BestSellers.tsx
@@ -13,7 +13,7 @@ import SEO from "@/components/SEO";
 import BamirFooter from "@/components/BamirFooter";
 
 const REFRESH_INTERVAL = 5 * 60 * 1000;
-const FREE_LIMIT = 4;
+const FREE_LIMIT = 1; // Ücretsiz 1 ürün göster, gerisi Pro
 const FREE_TRACK_LIMIT = 2;
 
 interface CJProduct {
